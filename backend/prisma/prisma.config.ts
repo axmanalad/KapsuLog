0e0326631299495251bc942eee61c7db09a0b677
axmanalad <alex.manalad8714@gmail.com>
2026-01-01 21:25:41 -0800

    feat: add Dockerfile and docker-compose for backend

---
+++ backend/prisma/prisma.config.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import type { PrismaConfig } from '@prisma/config';
+import { env } from '@prisma/config';
+
+export default {
+  datasource: {
+    url: env("DATABASE_URL")
+  },
+  engine: "classic"
+} satisfies PrismaConfig;
--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -185,6 +185,6 @@ main()
     console.error(e);
     process.exit(1);
   })
-  .finally(async () => {
-    await prisma.$disconnect();
+  .finally(() => {
+    void prisma.$disconnect();
   });
